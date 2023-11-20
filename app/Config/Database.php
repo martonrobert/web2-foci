3043bac9bc9c1667547a39b51b3a3fb366bef723
martonrobert <shxmtu@hallgato.uni-neumann.hu>
2023-11-20 22:37:45 +0100

    app config + routes table

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'root',
+        'password'     => 'R0bertG1da',
+        'database'     => 'foci',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,38 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+// Főoldal
+$routes->get('/', 'HomePageController::index');
+// Hírek API
+$routes->get('api-news', 'NewsApiGetController::index');
+
+
+// regisztráció form megjelenítése
+$routes->get('regisztrácio', 'RegisztracioPageController::index');
+// regisztráció mentése
+$routes->post('regisztracio', 'SignupApiController::index');
+
+
+// Bejelentkezés form megjelenítése
+$routes->get('belepes', 'LoginPageController::index');
+// Bejelentkezés API
+$routes->post('belepes', 'LoginApiController::index');
+// kilépő felület
+$routes->get('kilepes', 'LogoutPageController::index');
+
+
+
+// Csapatok lista nézet
+$routes->get('csapatok-lista', 'KlubListaViewController::index');
+// Csapatok lista lekérdezés API
+$routes->get('csapatok', 'KlubListaGetController::index');
+
+// Egy csapat adatainak megjelenítése, játékosokkal
+$routes->get('csapatok/(:num)','KlubViewController::index');
+// Csapat játékosainak lekélrdezése API
+$routes->get('csapatok/(:num)/jatekosok','JatekosokViewController::index');
+
+// Játékos adatainak mentése
+$routes->post('jatekos', 'JatekosPostController::index');
+
